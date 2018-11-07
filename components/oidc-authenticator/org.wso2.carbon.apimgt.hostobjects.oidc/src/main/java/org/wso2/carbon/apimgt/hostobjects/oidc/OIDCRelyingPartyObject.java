897d12b93c132741a82cd35bda99250115ad38d8
megala21 <ktumegala@yahoo.com>
2018-11-07 20:58:02 +0530

    Rename the username to usernameclaim

--- components/oidc-authenticator/org.wso2.carbon.apimgt.hostobjects.oidc/src/main/java/org/wso2/carbon/apimgt/hostobjects/oidc/OIDCRelyingPartyObject.java
+++ components/oidc-authenticator/org.wso2.carbon.apimgt.hostobjects.oidc/src/main/java/org/wso2/carbon/apimgt/hostobjects/oidc/OIDCRelyingPartyObject.java
@@ -388,7 +388,7 @@ public class OIDCRelyingPartyObject extends ScriptableObject {
 
         JsonObject jsonResponse = jsonRoot.getAsJsonObject();
 
-        String usernameProperty = relyingPartyObject.getOIDCProperty("username");
+        String usernameProperty = relyingPartyObject.getOIDCProperty("usernameclaim");
         if (StringUtils.isEmpty(usernameProperty)) {
             usernameProperty = "preferred_username";
         }
