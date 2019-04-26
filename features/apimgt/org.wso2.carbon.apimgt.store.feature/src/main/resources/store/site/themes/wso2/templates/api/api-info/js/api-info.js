8a5da7fb0500d2b76cb4f66e6aa5ea4cea1cdc6d
Hiranya Abeyrathne <hiranyaa@wso2.com>
2019-04-27 00:36:57 +0530

    Added application details for logging in the observability feature

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/templates/api/api-info/js/api-info.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/templates/api/api-info/js/api-info.js
@@ -20,7 +20,8 @@ function triggerSubscribe() {
         + '<span class="spinner"><i class="fw fw-loader5" title="button-loader"></i></span>'
     ).attr('disabled', 'disabled');
 
-    jagg.post("/site/blocks/subscription/subscription-add/ajax/subscription-add.jag", {
+    jagg.post("/site/blocks/subscription/subscription-add/ajax/subscription-add.jag?name=" + api.name + "&appId=" +
+    applicationId, {
         action:"addSubscription",
         applicationId:applicationId,
         name:api.name,
