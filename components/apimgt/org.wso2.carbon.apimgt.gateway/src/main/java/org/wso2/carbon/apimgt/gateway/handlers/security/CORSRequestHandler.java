04efb931ab8f0c72bf2a9d7165757e226ab663f8
Vithursa <vithu30@hotmail.com>
2019-09-18 14:40:02 +0530

    Add support to have wildcard character in CORS

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/CORSRequestHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/CORSRequestHandler.java
@@ -48,6 +48,8 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CORSRequestHandler extends AbstractHandler implements ManagedLifecycle {
 
@@ -335,6 +337,15 @@ public class CORSRequestHandler extends AbstractHandler implements ManagedLifecy
         } else if (allowedOrigins.contains(origin)) {
             return origin;
         } else {
+            for (String allowedOrigin : allowedOrigins) {
+                if (allowedOrigin.contains("*")) {
+                    Pattern pattern = Pattern.compile(allowedOrigin.replace("*", ".*"));
+                    Matcher matcher = pattern.matcher(origin);
+                    if (matcher.find()) {
+                        return origin;
+                    }
+                }
+            }
             return null;
         }
     }
